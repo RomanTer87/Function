c564f4d842fb67ae8d70360ac9ddc470e338812c
1 <1@DESKTOP-NU9I6RS>
2023-03-01 23:29:40 +0800

    Add project files.

---
+++ Functions/main.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+using namespace std;
+
+int Sum(int a, int b); // Прототип функции (Объявление функции - Function declaration
+int Dif(int a, int b);
+int Product(int a, int b);
+double Quotient(double a, double b);
+int Reminder(int a, int b);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	int a, b;
+	cout << "Введите два числа: "; cin >> a >> b;
+	
+	int c = Sum(a, b);  // использование функции (вызов функции, Function call)
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Dif(a,b) << endl;
+	cout << a << " * " << b << " = " << Product(a, b) << endl;
+	cout << a << " / " << b << " = " << Quotient(a, b) << endl;
+	cout << a << " % " << b << " = " << Reminder(a, b) << endl;
+}
+int Sum(int a, int b) // реализация функции (определение функции, Function definition)
+{
+	int c = a + b;
+	return c;
+}
+int Dif(int a, int b)
+{
+	//Difference - разность
+	return a - b;
+}
+int Product(int a, int b)
+{
+	//Product - произведение
+	return a * b;
+}
+double Quotient(double a, double b)
+{
+	//Quotient - частное
+	return a / b;
+}
+int Reminder(int a, int b)
+{
+	return a % b;
+}
---
+++ Homework_Function/main.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+using namespace std;
+
+//#define FACTORIAL
+//#define POWER
+
+#ifdef FACTORIAL
+int Factorial(int a, int f);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	int a;
+	int f = 1;
+	cout << "Введите предел факториала: "; cin >> a;
+	cout << "Значение: " << Factorial(a, f) << endl;
+}
+
+int Factorial(int a, int f = 1)
+{
+	for (int i = 1; i <= a; i++) f *= i;
+	return f;
+}
+#endif // FACTORIAL
+
+#ifdef POWER
+int Power(int a, int b);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	int a; // основание степени
+	int b; // показатель степени
+	int c = 1; // переменная для записи результата
+	cout << "Введите основание степени: "; cin >> a;
+	cout << "Введите показатель степени: "; cin >> b;
+	cout << "Полученный результат: " << Power(a, b) << endl;
+}
+int Power(int a, int b)
+{
+	int c = 1;
+	for (int i = 1; i <= b; i++) c *= a;
+	return c;
+}
+#endif // POWER
