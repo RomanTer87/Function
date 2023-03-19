136c13208ac1b25ae75104511a6aaefd8f1510a1
1 <1@DESKTOP-NU9I6RS>
2023-03-19 14:18:11 +0800

    new Homework

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,29 +1,158 @@
 #include<iostream>
 using namespace std;
 
+void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
+
+void Print(const int arr[], const int n);
+void Print(const double arr[], const int n);
+
+int sum(const int arr[], const int n);
+double Avg(const int arr[], const int n);
+int minValueIn(const int arr[], const int n);
+int maxValueIn(const int arr[], const int n);
+
+void Sort(int arr[], const int n);
+
+void shiftLeft(int arr[], const int n, int number_of_shifts);
+void shiftRight(int arr[], const int n, int number_of_shifts);
+
 void main()
 {
 	setlocale(LC_ALL, "");
 
-	const int n = 5;
-	int arr[n] = {};
-	//arr[2] = 123;   // обращение ко второму элементу массива на запись
-	//cout << arr[2] << endl;  // обращение ко второму элементу массива на чтение
-	//SIZE-1;
-	//   n-1;
+	const int n = 10;
+	int arr[n] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	FillRand(arr, n);
+	Print(arr, n);
+	cout << "Суума элементов массива: " << sum(arr, n) << endl;
+	cout << "Среднее арифметическое значение: " << Avg(arr, n) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(arr, n) << endl;
+	cout << "Маскимальное значение в массиве:" << maxValueIn(arr, n) << endl;
+	Sort(arr, n);
+	Print(arr, n);
 
-	//Ввод массива с клавиатуры
-	cout << "Введите элементы массива: ";
+	/*int number_of_shifts;
+	cout << "Введите количество сдвигов "; cin >> number_of_shifts;
+	shiftLeft(arr, n, number_of_shifts);
+	Print(arr, n);
+	shiftRight(arr, n, number_of_shifts);
+	Print(arr, n);*/
+
+	const int SIZE = 8;
+	double d_arr[SIZE];
+	FillRand(d_arr, SIZE);
+	Print(d_arr, SIZE);
+}
+//rand(); // возвращает псевдослучайное число в диапазоне от 0 до 32 767 (MAX_RAND)
+void FillRand(int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
-		cin >> arr[i];
+		arr[i] = rand() % 1000;
+		arr[i] /= 100;
 	}
-
+}
+void FillRand(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+void Print(const int arr[], const int n)
+{
 	//Вывод массива на экран:
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
-
+}
+void Print(const double arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+int sum(const int arr[], const int n)
+{
+	//Вычисление суммы элементов массива:
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+double Avg(const int arr[], const int n)
+{
+	return (double)sum(arr, n) / n;
+}
+int minValueIn(const int arr[], const int n)
+{
+	int min;
+	min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min) min = arr[i];
+	}
+	return min;
+}
+int maxValueIn(const int arr[], const int n)
+{
+	int max;
+	max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max) max = arr[i];
+	}
+	return max;
+}
+void shiftLeft(int arr[], const int n, int number_of_shifts)
+{
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = buffer;
+	}
+}
+void shiftRight(int arr[], const int n, int number_of_shifts)
+{
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		/*int buffer = arr[n-1];
+		for (int i = n-1; i > 0; i--)
+		{
+			arr[i] = arr[i-1];
+		}
+		arr[0] = buffer;*/
+		shiftLeft(arr, n, n - number_of_shifts);
+	}
+}
+void Sort(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//счетчик i выбирает элемент в который нужно поместить минимальное значение из перебираемых
+		for (int j = i + 1; j < n; j++)
+		{
+			// счетчик j перебирает элементы в поисках минимального
+			//arr[i] - выбранный элемент
+			//arr[j] - перебираемый элемент
+			if (arr[j] < arr[i])
+			{
+				arr[i] ^= arr[j];
+				arr[j] ^= arr[i];
+				arr[i] ^= arr[j];
+			}
+		}
+	}
 }
--- Practice/main.cpp
+++ Practice/main.cpp
@@ -1,16 +1,26 @@
 ﻿#include<iostream>
 using namespace std;
 
-int SUMM(int a, int b);
-int DIFF(int a, int b);
-int UMNOZH(int a, int b);
-double DELENIE(double a, double b);
-int  FACTORIAL(int a);
-int STEPEN(int a, int b);
+//int SUMM(int a, int b);
+//int DIFF(int a, int b);
+//int UMNOZH(int a, int b);
+//double DELENIE(double a, double b);
+//int  FACTORIAL(int a);
+//int STEPEN(int a, int b);
 
 void main()
 {
 	setlocale(LC_ALL, "");
+
+	int a;
+	int b;
+	cout << "Введите значения переменной a: "; cin >> a;
+	cout << "Введите значения переменной b: "; cin >> b;
+	a ^= b;
+	b ^= a;
+	a ^= b;
+	cout << "Значение переменной a после операции XOR: " << a << endl;
+	cout << "Значение переменной b после операции XOR: " << b << endl;
 
 	/*int a, b;
 	cout << "Введите два числа: "; cin >> a >> b;
@@ -23,41 +33,41 @@ void main()
 	cout << "Введите основание степени и степень: "; cin >> a >> b;
 	cout << "Результат возведения в степень: " << STEPEN(a, b) << endl;*/
 }
-int SUMM(int a, int b)
-{
-	int c = a + b;
-	return c;
-}
-int DIFF(int a, int b)
-{
-	int c = a - b;
-	return c;
-}
-int UMNOZH(int a, int b)
-{
-	int c = a * b;
-	return c;
-}
-double DELENIE(double a, double b)
-{
-	double c = a / b;
-	return c;
-}
-int FACTORIAL(int a)
-{
-	int f = 1;
-	for (int i = 1; i <= a; i++)
-	{
-		f *= i;
-	}
-	return f;
-}
-int STEPEN(int a, int b)
-{
-	int s = 1;
-	for (int i = 0; i < b; i++)
-	{
-		s *= a;
-	}
-	return s;
-}
+//int SUMM(int a, int b)
+//{
+//	int c = a + b;
+//	return c;
+//}
+//int DIFF(int a, int b)
+//{
+//	int c = a - b;
+//	return c;
+//}
+//int UMNOZH(int a, int b)
+//{
+//	int c = a * b;
+//	return c;
+//}
+//double DELENIE(double a, double b)
+//{
+//	double c = a / b;
+//	return c;
+//}
+//int FACTORIAL(int a)
+//{
+//	int f = 1;
+//	for (int i = 1; i <= a; i++)
+//	{
+//		f *= i;
+//	}
+//	return f;
+//}
+//int STEPEN(int a, int b)
+//{
+//	int s = 1;
+//	for (int i = 0; i < b; i++)
+//	{
+//		s *= a;
+//	}
+//	return s;
+//}
